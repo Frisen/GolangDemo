d751304b904af5ca49b48ebd48321aeb5f7f9fea
zhangxinshuai <zhangxinshuai24@outlook.com>
2020-06-18 23:57:36 +0800

    获取相同输入行并输出

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
-//echo输出命令行参数
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	fmt.Println(strings.Join(os.Args[0:], " "))
+	//fmt.Println(strings.Join(os.Args[0:], " "))
+	dup1()
+}
+
+//从命令行获取输入的包bufio
+//获取相同行
+func dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() == "exit" {
+			break
+		}
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
