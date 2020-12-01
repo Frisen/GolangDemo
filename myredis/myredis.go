b75ac1ff362bec14e3d53ae9b23be5455854d942
zhangxinshuai <zhangxinshuai24@outlook.com>
2020-12-01 23:00:37 +0800

    redis

--- myredis/myredis.go
+++ myredis/myredis.go
@@ -3,11 +3,14 @@ package myredis
 import (
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	"github.com/go-redis/redis"
 )
 
 var client *redis.Client
+var count int
 
 func init() {
 	client = redis.NewClient(&redis.Options{
@@ -32,11 +35,89 @@ func ConnectRedis() {
 
 //SetKey ...
 func SetKey(k, v string) {
-	client.Set(k, v, 0)
+	client.Set(k, v, 0).Err()
 }
 
 //GetKey ...
 func GetKey(k string) string {
-	strcmd := client.Get(k)
-	return strcmd.Val()
+	ret, err := client.Get(k).Result()
+	if err == redis.Nil {
+		fmt.Printf("name do not exist")
+		return ""
+	} else if err != nil {
+		fmt.Printf("get name faild err:%v\n", err)
+		return ""
+	} else {
+		return ret
+	}
+}
+
+// Add ...
+func Add() {
+	var wg sync.WaitGroup
+	var l sync.Mutex
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			defer wg.Done()
+			l.Lock()
+			count++
+			l.Unlock()
+		}()
+	}
+	wg.Wait()
+	println(count)
+}
+
+//AddWithRedisLock ...
+func AddWithRedisLock() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		time.Sleep(time.Millisecond * 60)
+		go func() {
+			defer wg.Done()
+			incr()
+		}()
+	}
+	wg.Wait()
+}
+
+func incr() {
+	lockKey := "count_key"
+	countKey := "counter"
+	//加锁
+	//defer client.Close()
+	cmdLock := client.SetNX(lockKey, 1, time.Second*3)
+	issucceed, err := cmdLock.Result()
+	if err != nil || !issucceed {
+		//client.Del(lockKey)
+		fmt.Println("创建锁失败", err)
+		return
+	}
+	//获取数值
+	cmdGet := client.Get(countKey)
+	v, err := cmdGet.Int64()
+	if err != nil {
+		//client.Del(lockKey)
+		fmt.Println("get失败", err)
+	}
+	v++
+
+	//设置数值
+	cmdSet := client.Set(countKey, v, 0)
+	if _, err := cmdSet.Result(); err != nil {
+		//client.Del(lockKey)
+		println("set value error!")
+	}
+
+	println("current counter is ", v)
+
+	delResp := client.Del(lockKey)
+	unlockSuccess, err := delResp.Result()
+	if err == nil && unlockSuccess > 0 {
+		println("unlock success!")
+	} else {
+		println("unlock failed")
+	}
 }
--- myredis/myredis_test.go
+++ myredis/myredis_test.go
@@ -8,7 +8,15 @@ import (
 
 func Test_ConnectRedis(t *testing.T) {
 	myredis.ConnectRedis()
-	myredis.SetKey("huang.xinhui", "我是黄馨慧")
-	v := myredis.GetKey("huang.xinhui")
+	myredis.SetKey("huang", "黄圆圆0001")
+	v := myredis.GetKey("huang")
 	fmt.Println("getkey value ---------->", v)
+}
+
+func Test_Add(t *testing.T) {
+	myredis.Add()
+}
+
+func Test_AddWithRedisLock(t *testing.T) {
+	myredis.AddWithRedisLock()
 }
