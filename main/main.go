050fe17b84ed61b0a98826434664db22030dd2f3
zhangxinshuai <zhangxinshuai24@outlook.com>
2020-12-08 00:04:23 +0800

    golang my stack 实现

--- main/main.go
+++ main/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	datastruct "GolangDemo/data_struct"
 	"bufio"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type person struct {
@@ -27,7 +29,35 @@ type Person struct {
 var mode = flag.String("mode", "", "process mode")
 
 func main() {
+	fmt.Print("Enter text: \n")
+	reader := bufio.NewReader(os.Stdin)
+	// ReadString will block until the delimiter is entered
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+	// remove the delimeter from the string
+	input = strings.TrimSuffix(input, "\n")
+	fmt.Println(input)
 
+	charMap := map[byte]byte{'(': ')', '[': ']', '{': '}'}
+	count := len([]rune(input))
+	stack := datastruct.NewStack()
+	for i := 0; i < count; i++ {
+		if stack.Length() > 0 {
+			if v, ok := charMap[stack.Peek().(byte)]; ok && v == input[i] {
+				stack.Pop()
+				continue
+			}
+		}
+		stack.Push(input[i])
+	}
+	if stack.Length() > 0 {
+		fmt.Println("not matched!")
+	} else {
+		fmt.Println("matched!")
+	}
 	return
 	stop := make(chan interface{})
 	//ch := make(chan int, 3)
